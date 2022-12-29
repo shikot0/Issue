18cc9c6ee943257584fefebd363918b740d290ab
shikoto <1sh1m1da1@gmail.com>
2022-12-29 01:45:39 +0000

    test

--- app/src/utils/APIRoutes.js
+++ app/src/utils/APIRoutes.js
@@ -1,6 +1,6 @@
-// export const host  = 'http://localhost:5000';
+export const host  = 'http://localhost:5000';
 require('dotenv').config()
-const host = process.env.SERVER;
+// const host = process.env.SERVER;
 export const loginRoute = `${host}/api/user/login`;
 export const registerRoute = `${host}/api/user/register`;
 export const getUserRoute = `${host}/api/user/getuser`;
